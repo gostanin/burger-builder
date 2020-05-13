a48b634fe09e253662da7a27bed2391bd8350210
Gregory Ostanin <ostingreg@gmail.com>
2020-05-12 18:01:00 -0700

    Set up top level components

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    cheese: 0.4,
+    meat: 1.3,
+    bacon: 0.7
+}
+
+
+class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
+    }
+
+    addIngredientHandler = (type) => {
+        const oldCount = this.state.ingredients[type];
+        const updatedCount = oldCount + 1;
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = updatedCount;
+        const priceAddition = INGREDIENT_PRICES[type];
+        const oldPrice = this.state.totalPrice;
+        const newPrice = oldPrice + priceAddition;
+        this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    }
+
+    removeIngredientHandler = (type) => {
+        const ingredients = {...this.state.ingredients};
+        const currentAmount = ingredients[type]
+        let price = this.state.totalPrice
+        if (currentAmount > 0) {
+            ingredients[type] -= 1;
+            price = price - INGREDIENT_PRICES[type];
+        }
+        this.setState({totalPrice: price, ingredients: ingredients})
+    }
+
+    render () {
+        const disabledInfo ={
+            ...this.state.ingredients
+        };
+        for (let key in disabledInfo) {
+            disabledInfo[key] = disabledInfo[key] <= 0
+        }
+        return (
+            <React.Fragment>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler}    
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={disabledInfo}
+                    price={this.state.totalPrice}
+                />
+            </React.Fragment>
+        );
+    }
+}
+
+export default BurgerBuilder
