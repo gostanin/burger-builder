477a72a08f0cbe8685b2275ef2383fe65a4cc5cd
Gregory Ostanin <ostingreg@gmail.com>
2020-07-04 19:52:51 -0700

    auth test

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,38 @@
+import reducer from './auth';
+import * as actionTypes from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+    it('should return the initial state', () => {
+        expect(reducer(undefined, {})).toEqual(
+            { 
+                token: null,
+                userId: null,
+                error: null,
+                loading: false,
+                authRedirect: '/'
+            })
+    })
+
+    it('should store the login upon a login', () => {
+        expect(reducer({ 
+            token: null,
+            userId: null,
+            error: null,
+            loading: false,
+            authRedirect: '/'
+        },
+        {
+            type: actionTypes.AUTH_SUCCESS,
+            token: 'some-token',
+            userId: 'some-user-id'
+
+        }
+        )).toEqual({ 
+            token: 'some-token',
+            userId: 'some-user-id',
+            error: null,
+            loading: false,
+            authRedirect: '/'
+        })
+    });
+})
