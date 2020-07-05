8a65a0195d81ed6479d7738a1a5cad7e1403233d
Gregory Ostanin <ostingreg@gmail.com>
2020-07-05 08:34:23 -0700

    Fix bugs

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -59,7 +59,7 @@ class Auth extends Component {
             [controlName]: 
                 updateObject(this.state.controls[controlName],
                 {value: event.target.value,
-                valid: this.checkValidation(event.target.value, this.state.controls[controlName].validation),
+                valid: checkValidation(event.target.value, this.state.controls[controlName].validation),
                 touched: true})
         })
         this.setState({controls: updatedControls})
